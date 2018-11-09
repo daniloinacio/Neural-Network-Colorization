d61b71f3f4b115d445a48f9eb9ede029dae530ba
Felipe Lima <felipesoueu@live.com>
2018-11-08 23:47:45 -0200

    Pastas Reorganizadas

--- Redes Neurais + SOM (Artigo)/SOM.py
+++ Redes Neurais + SOM (Artigo)/SOM.py
@@ -22,7 +22,7 @@ if __name__ == '__main__':
 	dataset = reduced_data
 
 	# Criação da SOM
-	som = sompy.SOMFactory.build(dataset, mapsize=[3, 3], mask=None, mapshape='planar',
+	som = sompy.SOMFactory.build(dataset, mapsize=[200, 200], mask=None, mapshape='planar',
 	 lattice='rect', normalization='var', initialization='pca', neighborhood='gaussian', training='batch', name='coloring')
 
 	som.train(n_job=1, verbose='info', train_rough_len=None, train_finetune_len=None)
@@ -38,12 +38,12 @@ if __name__ == '__main__':
 	new_data = np.uint8(codebook[proj])
 
 	# Cria um vetor de regiões do L
-	train_input = fd.image_mapping(img_LUV[:, :, 0].reshape(img_LUV.shape[0] * img_LUV.shape[1], 1), 3)
+	train_input = fd.image_mapping(img_LUV[:, :, 0], 3)
 	train_target = new_data
 
 	# Cria uma rede pro U e uma pro V
-	clf_U = MLPClassifier(activation='relu', hidden_layer_sizes=(200, 200,), max_iter=1000, learning_rate_init=0.001, verbose=True)
-	clf_V = MLPClassifier(activation='relu', hidden_layer_sizes=(200, 200,), max_iter=1000, learning_rate_init=0.001, verbose=True)
+	clf_U = MLPClassifier(activation='relu', hidden_layer_sizes=(20, 20,), max_iter=1000, learning_rate_init=0.001, verbose=True)
+	clf_V = MLPClassifier(activation='relu', hidden_layer_sizes=(20, 20,), max_iter=1000, learning_rate_init=0.001, verbose=True)
 	
 	clf_U.fit(train_input, train_target[:, 0])
 	clf_V.fit(train_input, train_target[:, 1])
@@ -53,18 +53,17 @@ if __name__ == '__main__':
 	accuracy_V = 100 * clf_V.score(train_input, train_target[:, 1])
 	print('Accuracy U: %.2lf' % accuracy_U)
 	print('Accuracy V: %.2lf' % accuracy_V)	
-	'''
-	UV = np.concatenate((U, V), 1)
-	new_img = np.concatenate((img_grey.reshape(img.shape[0] * img.shape[1], 1), UV), 1)
-	new_img = new_img.reshape(img.shape[0], img.shape[1], 3)
+	
+	new_U = U.reshape(img.shape[0], img.shape[1], 1)
+	new_V = V.reshape(img.shape[0], img.shape[1], 1)
+
+	new_img = cv2.merge([img_grey, new_U, new_V])
 	result = cv2.cvtColor(np.uint8(new_img), cv2.COLOR_LUV2BGR)
-	new_data = new_data.reshape(img.shape[0], img.shape[1], 2)
 	cv2.imshow('original U', img_UV[:, :, 0])
-	cv2.imshow('predict U', np.uint8(new_data[:, :, 0]))
+	cv2.imshow('predict U', np.uint8(new_U))
 	cv2.imshow('original V', img_UV[:, :, 1])
-	cv2.imshow('predict V', np.uint8(new_data[:, :, 1]))
-	cv2.imshow('origigal', img)
-	cv2.imshow('test', result)
-	'''
+	cv2.imshow('predict V', np.uint8(new_V))
+	cv2.imshow('original', img)
+	cv2.imshow('test', result)	
 	cv2.waitKey(0)
 	cv2.destroyAllWindows()
